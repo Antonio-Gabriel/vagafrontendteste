a63be4ee4d1494084c343dcbb550f623f42747d0
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-02 00:57:32 +0100

    btn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider, DefaultTheme } from "styled-components";
 
+import { AuthContextProvider } from "./provider/AuthContext";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
+
 import light from "./styles/themes/light";
 import dark from "./styles/themes/dark";
 
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { useDarkMode } from "./hooks/useDarkMode";
 
-import { Auth } from "./pages/Auth";
+import { Routes } from "./routes";
 
 import "./assets/icons/style.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export function App() {
   const [theme, setTheme] = useDarkMode<DefaultTheme>("theme", light);
@@ -18,9 +23,14 @@ export function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Auth toggleTheme={toggleTheme} />
-      <GlobalStyles />
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes toggleTheme={toggleTheme} />
+        </BrowserRouter>
+        <GlobalStyles />
+        <ToastContainer />
+      </ThemeProvider>
+    </AuthContextProvider>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,18 +3,17 @@ import { Container } from "./styles";
 type IbuttonProps = {
   width: number;
   height: number;
-  text: string;
-  handler: () => void;
+  text: string;  
 };
 
-export function Button({ width, height, text, handler }: IbuttonProps) {
+export function Button({ width, height, text }: IbuttonProps) {
   return (
     <Container width={width} height={height}>
-      <button type="submit" onClick={handler}>
+      <button type="submit">
         <div className="circle">
           <i className="icon-plus" />
         </div>
-        {text}
+        { text }
       </button>
     </Container>
   );
