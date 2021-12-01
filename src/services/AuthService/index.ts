041d48d8888353b03629bbdbb30e985ac8a174f8
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-02 00:55:58 +0100

    auth service

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,7 +1,17 @@
 import { IAuthentication } from "../../types/IAuthentication";
 
-export function AuthService({ username, password }: IAuthentication) {
+export function AuthService(data: IAuthentication) {
+  if (data.username === "Admin" && data.password === "admin") {
+    localStorage.setItem(
+      "auth",
+      JSON.stringify({
+        user: data.username,
+        created_at: new Date(Date.now()),
+      })
+    );
 
-  
+    return true;
+  }
 
+  return false;
 }
