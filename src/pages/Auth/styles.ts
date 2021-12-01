afa0635e984add13b68f73c5ac2007cf0a26bab3
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-02 00:56:52 +0100

    auth style

--- src/pages/Auth/styles.ts
+++ src/pages/Auth/styles.ts
@@ -20,6 +20,8 @@ export const Container = styled.div`
       display: flex;
       align-items: center;
       justify-content: center;
+      
+      cursor: pointer;
 
       i {
         font-size: 1rem;
