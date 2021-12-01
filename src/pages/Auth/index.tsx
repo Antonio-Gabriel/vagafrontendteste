132037b8c88862b020e9abcf2c741617eb1e5f06
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-02 00:57:05 +0100

    authentication role

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,84 +1,106 @@
 import { Container, Content, Form, Header } from "./styles";
 
-import { Input } from "../../components/Input/index";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
+import { IAuthentication } from "../../types/IAuthentication";
+import { useAuth } from "../../hooks/useAuth";
 import { Button } from "../../components/Button";
+import { Input } from "../../components/Input/index";
+
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 interface IPropsTheme {
   toggleTheme: () => void;
 }
 
 export function Auth({ toggleTheme }: IPropsTheme) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { signInUser } = useAuth();
+  let history = useHistory();
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
+  const [data, setData] = useState<IAuthentication>({
+    username: "",
+    password: "",
+  });
 
-    const data = {
-      username,
-      password,
-    };
-
-    console.log(data);
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
   }
 
-  function AuthUser() {
-    console.log("click");
+  function handleAuthUser(event: ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    try {
+      const isAuth = signInUser(data);
+      if (isAuth) {
+        toast.success("Authorized");
+
+        setData({
+          username: "",
+          password: "",
+        });
+
+        history.push("/dashboard");
+      } else {
+        toast.error("Email or password invalid!");
+
+        setData({
+          username: "",
+          password: "",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <>
-      <Container>
-        <header>
-          <div className="translate">
-            <i className="icon-g_translate"></i>
-          </div>
-          <div className="theme" onClick={toggleTheme}>
-            <i className="icon-dark"></i>
-          </div>
-        </header>
-        <Content>
-          <div className="text">
-            <h1>Faça o Login para continuar a navegar na plataforma</h1>
-            <p>É bom ter você de volta.</p>
-          </div>
-          <div className="auth">
-            <body className="card">
-              <Header>
-                <h2>Wiid.FrontEnd</h2>
-              </Header>
+    <Container>
+      <header>
+        <div className="translate">
+          <i className="icon-g_translate"></i>
+        </div>
+        <div className="theme" onClick={toggleTheme}>
+          <i className="icon-dark"></i>
+        </div>
+      </header>
+      <Content>
+        <div className="text">
+          <h1>Faça o Login para continuar a navegar na plataforma</h1>
+          <p>É bom ter você de volta.</p>
+        </div>
+        <div className="auth">
+          <div className="card">
+            <Header>
+              <h2>Wiid.FrontEnd</h2>
+            </Header>
 
-              <Form onSubmit={handleSubmit}>
-                <Input
-                  name="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  placeholder="Username"
-                  type="text"
-                  required
-                />
-                <Input
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Password"
-                  isPassword
-                  type="password"
-                  required
-                />
+            <Form onSubmit={handleAuthUser}>
+              <Input
+                name="username"
+                value={data.username}
+                onChange={handleInputChange}
+                placeholder="Username"
+                type="text"
+                required
+              />
+              <Input
+                name="password"
+                value={data.password}
+                onChange={handleInputChange}
+                placeholder="Password"
+                isPassword
+                type="password"
+                required
+              />
 
-                <Button
-                  text="login"
-                  width={100}
-                  height={4}
-                  handler={AuthUser}
-                />
-              </Form>
-            </body>
+              <Button text="login" width={100} height={4} />
+            </Form>
           </div>
-        </Content>
-      </Container>
-    </>
+        </div>
+      </Content>
+    </Container>
   );
 }
